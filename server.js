0e0e88bc30da2d3485afee6b066c068d219dfc58
aakash-sharma25 <as024harma@gmail.com>
2024-02-19 20:12:10 +0530

    Production setup

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use("/api/v1/category" , categoryRoutes)
 app.use("/api/v1/product" , productRoute)
 
 
-app.use(express.static(path.join(__dirname,'./client/build')));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-app.get('*',(req,res)=>{
-    res.sendFile((path.join(__dirname,'./client/build/index.html')))
+app.get("*", function (req,res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"))
 });
 
 const PORT = process.env.PORT ;
