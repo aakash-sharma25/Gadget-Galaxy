99f19cc51dd62af0f9c2a6f895b60a4249c1a5a2
aakash-sharma25 <as024harma@gmail.com>
2024-02-19 19:51:38 +0530

    Production setup

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const authRoute = require("./routes/authRoute");
 const categoryRoutes = require("./routes/categoryRoutes");
 const productRoute = require("./routes/productRoute");
 require("dotenv").config();
+const path = require("path");
 
 
-const PORT = process.env.PORT ;
+
 
 const app = express();
 app.use(cors());
@@ -25,15 +26,13 @@ app.use("/api/v1/category" , categoryRoutes)
 app.use("/api/v1/product" , productRoute)
 
 
+app.use(expre.static(path.join(__dirname,'./client/build')));
 
+app.get('*',(req,res)=>{
+    res.sendFile((path.join(__dirname,'./client/build/index.html')))
+});
 
-
-app.get("/", (req,res)=>{
-    res.send("<h1>hello ecommerce</h1>")
-})
-
-
-
+const PORT = process.env.PORT ;
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
